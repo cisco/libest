86e0a6165f1dd158ab726167f6b898ea8f717caa
David von Oheimb <David.von.Oheimb@siemens.com>
2014-06-24 15:16:34 +0100

    removed default -v option; added full flexiblity for user-supplied options

--- example/server/runserver.sh
+++ example/server/runserver.sh
@@ -6,4 +6,4 @@ export EST_CACERTS_RESP=./estCA/cacert.crt
 export EST_OPENSSL_CACONFIG=./estExampleCA.cnf
 #export EST_CSR_ATTR="MGwGBysGAQEBARYwIgYDiDcBMRsTGVBhcnNlIFNFVCBhcyAyLjk5OS4xIGRhdGEwJwYDiDcCMSAGA4g3AxMZUGFyc2UgU0VUIGFzIDIuOTk5LjIgZGF0YQYJKyQDAwIIAQELBglghkgBZQMEAgI="
 
-./estserver -c estCA/private/estservercertandkey.pem -k estCA/private/estservercertandkey.pem -r estrealm -v
+./estserver -c estCA/private/estservercertandkey.pem -k estCA/private/estservercertandkey.pem -r estrealm $@
