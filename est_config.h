196a32c965dec27aa915ddbf082c36ac5d7fdb63
Megh Dholakia <mdholaki@ubuntu.localdomain>
2015-06-26 11:22:44 -0700

    Client will not retry an enrollment attempt when no http authentication is provided

---
+++ est_config.h
@@ -0,0 +1,25 @@
+/* confdefs.h */
+#define PACKAGE_NAME "libest"
+#define PACKAGE_TARNAME "libest"
+#define PACKAGE_VERSION "1.1.1p"
+#define PACKAGE_STRING "libest 1.1.1p"
+#define PACKAGE_BUGREPORT "libest-dev"
+#define PACKAGE_URL ""
+#define PACKAGE "libest"
+#define VERSION "1.1.1p"
+#define STDC_HEADERS 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRING_H 1
+#define HAVE_MEMORY_H 1
+#define HAVE_STRINGS_H 1
+#define HAVE_INTTYPES_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_UNISTD_H 1
+#define HAVE_DLFCN_H 1
+#define LT_OBJDIR ".libs/"
+#define HAVE_LIBDL 1
+#define HAVE_LIBPTHREAD 1
+#define HAVE_LIBCRYPTO 1
+#define HAVE_LIBSSL 1
